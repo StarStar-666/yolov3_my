9cb086d85d8952cd90da6d01a1117fd7463ef4b9
StarStar-666 <74656691+StarStar-666@users.noreply.github.com>
2021-04-01 13:20:31 +0800

    Add files via upload

---
+++ YOLOV3/core/backbone.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : backbone.py
+#   Author      : YunYang1994
+#   Created date: 2019-07-11 23:37:51
+#   Description :
+#
+#================================================================
+
+import tensorflow as tf
+import core.common as common
+
+
+def darknet53(input_data):
+
+    input_data = common.convolutional(input_data, (3, 3,  3,  32))
+    input_data = common.convolutional(input_data, (3, 3, 32,  64), downsample=True)
+
+    for i in range(1):
+        input_data = common.residual_block(input_data,  64,  32, 64)
+
+    input_data = common.convolutional(input_data, (3, 3,  64, 128), downsample=True)
+
+    for i in range(2):
+        input_data = common.residual_block(input_data, 128,  64, 128)
+
+    input_data = common.convolutional(input_data, (3, 3, 128, 256), downsample=True)
+
+    for i in range(8):
+        input_data = common.residual_block(input_data, 256, 128, 256)
+
+    route_1 = input_data
+    input_data = common.convolutional(input_data, (3, 3, 256, 512), downsample=True)
+
+    for i in range(8):
+        input_data = common.residual_block(input_data, 512, 256, 512)
+
+    route_2 = input_data
+    input_data = common.convolutional(input_data, (3, 3, 512, 1024), downsample=True)
+
+    for i in range(4):
+        input_data = common.residual_block(input_data, 1024, 512, 1024)
+
+    return route_1, route_2, input_data
+
+def _stem_block(input_x):
+    conv0 = common.convolutional(input_x,(3,3,3,32), downsample=True, activate=True, bn=True)
+    conv1_l0 = common.convolutional(conv0, (1, 1, 32, 16), downsample=False, activate=True, bn=True)
+    conv1_l1 = common.convolutional(conv1_l0 , (3, 3, 16, 32), downsample=True, activate=True, bn=True)
+    maxpool1_r0 = tf.keras.layers.Max2D(conv1_l1, pool_size=(2, 2), strides=(2,2), padding'=SAME')
+    filter_concat = tf.concat([conv1_l1, maxpool1_r0], axis=-1)
+    output = common.convolutional(filter_concat, (1, 1, 32, 32), downsample=True, activate=True, bn=True)
+    return output
+
+def _dense_block(input_x, stage,num_block, k, bottleneck_width):
+    output = input_x
+    for index in range(num_block):
+        inter_channel = k * bottleneck_width
+        # left channel
+        conv_left_0 = common.convolutional(input_x, (1, 1,tf.cast(tf.shape(input_x)[-1], tf.int32) ,inter_channel), downsample=False, activate=True, bn=True)
+        conv_left_1 = common.convolutional(conv_left_0, (3, 3, inter_channel,k), downsample=False, activate=True, bn=True)
+        # right channel
+        conv_right_0= common.convolutional(input_x, (1, 1,tf.cast(tf.shape(input_x)[-1], tf.int32) ,inter_channel), downsample=False, activate=True, bn=True)
+        conv_right_1= common.convolutional(conv_right_0, (3, 3, inter_channel,k), downsample=False, activate=True, bn=True)
+        conv_right_2= common.convolutional(conv_right_1, (3, 3, k , k), downsample=False, activate=True, bn=True)
+
+        output = tf.concat([output, conv_left_1, conv_right_2], axis=3)
+
+    return output
+
+def _transition_layer(input_x, stage,output_channel, is_avgpool=True):
+
+    conv0 = common.convolutional(input_x, (1, 1,tf.cast(tf.shape(input_x)[-1], tf.int32) ,output_channel), downsample=False, activate=True, bn=True)
+
+    if is_avgpool:
+       conv0 =  common.convolutional(input_x, (1, 1, output_channel, output_channel),downsample=True, activate=True, bn=True)
+       output = conv0
+
+    return output
+
+
+
+
+
+
+
+
+def peleenet(input_data):
+
+    stem_block_output = _stem_block(input_data)
+
+    dense_block_output = _dense_block(stem_block_output, 0, 3, 16, 2)
+
+    transition_layer_output =_transition_layer(dense_block_output, 0, 128)
+
+    dense_block_output1 = _dense_block(transition_layer_output, 1, 4, 16, 2)
+
+    transition_layer_output1 = _transition_layer(dense_block_output1, 1, 256)
+
+    dense_block_output2 = _dense_block(transition_layer_output1, 2, 8, 16, 4)
+
+    transition_layer_output2 = _transition_layer(dense_block_output2, 2, 512)
+
+    dense_block_output3 =_dense_block(transition_layer_output2, 3, 6, 16, 4)
+
+    transition_layer_output3 = _transition_layer(dense_block_output3, 3, 1024, is_avgpool=False)
+
+    return dense_block_output1, dense_block_output2, transition_layer_output3
+
+
+
+
+
---
+++ YOLOV3/core/common.py
@@ -0,0 +1,194 @@
+#! /usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2019 * Ltd. All rights reserved.
+#
+#   Editor      : VIM
+#   File name   : common.py
+#   Author      : YunYang1994
+#   Created date: 2019-07-11 23:12:53
+#   Description :
+#
+#================================================================
+
+import tensorflow as tf
+
+class BatchNormalization(tf.keras.layers.BatchNormalization):
+    """
+    "Frozen state" and "inference mode" are two separate concepts.
+    `layer.trainable = False` is to freeze the layer, so the layer will use
+    stored moving `var` and `mean` in the "inference mode", and both `gama`
+    and `beta` will not be updated !
+    """
+    def call(self, x, training=False):
+        if not training:
+            training = tf.constant(False)
+        training = tf.logical_and(training, self.trainable)
+        return super().call(x, training)
+
+def convolutional(input_layer, filters_shape, downsample=False, activate=True, bn=True):
+    if downsample:
+        input_layer = tf.keras.layers.ZeroPadding2D(((1, 0), (1, 0)))(input_layer)
+        padding = 'valid'
+        strides = 2
+    else:
+        strides = 1
+        padding = 'same'
+
+    conv = tf.keras.layers.Conv2D(filters=filters_shape[-1], kernel_size = filters_shape[0], strides=strides, padding=padding,
+                                  use_bias=not bn, kernel_regularizer=tf.keras.regularizers.l2(0.0005),
+                                  kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                  bias_initializer=tf.constant_initializer(0.))(input_layer)
+
+    if bn: conv = BatchNormalization()(conv)
+    if activate == True: conv = tf.nn.leaky_relu(conv, alpha=0.1)
+
+    return conv
+
+
+def convolutional(input_layer, filters_shape, downsample=False, activate=True, bn=True):
+    if downsample:
+        input_layer = tf.keras.layers.ZeroPadding2D(((1, 0), (1, 0)))(input_layer)
+        padding = 'valid'
+        strides = 2
+    else:
+        strides = 1
+        padding = 'same'
+
+    conv = tf.keras.layers.Conv2D(filters=filters_shape[-1], kernel_size = filters_shape[0], strides=strides, padding=padding,
+                                  use_bias=not bn, kernel_regularizer=tf.keras.regularizers.l2(0.0005),
+                                  kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                  bias_initializer=tf.constant_initializer(0.))(input_layer)
+
+    if bn: conv = BatchNormalization()(conv)
+    if activate == True: conv = tf.nn.leaky_relu(conv, alpha=0.1)
+
+    return conv
+
+def conv2d_bn(x,filters,num_row,num_col,padding='same',stride=1,dilation_rate=1,relu=True):
+    x =  tf.keras.layers.Conv2D(
+        filters, (num_row, num_col),
+        strides=(stride,stride),
+        padding=padding,
+        kernel_regularizer=tf.keras.regularizers.l2(0.0005),
+        dilation_rate=(dilation_rate, dilation_rate),
+        use_bias=False)(x)
+    x = BatchNormalization()(x)
+    if relu:
+        x = tf.nn.leaky_relu(x, alpha=0.1)
+    return x
+
+def residual_block(input_layer, input_channel, filter_num1, filter_num2):
+    short_cut = input_layer
+    conv = convolutional(input_layer, filters_shape=(1, 1, input_channel, filter_num1))
+    conv = convolutional(conv , filters_shape=(3, 3, filter_num1,   filter_num2))
+
+    residual_output = short_cut + conv
+    return residual_output
+
+def upsample(input_layer):
+    return tf.image.resize(input_layer, (input_layer.shape[1] * 2, input_layer.shape[2] * 2), method='nearest')
+
+
+
+def RFB_net(input_data ,trainable, name, stride=1,scale =0.1, activate=True, bn=True):
+
+
+    conv =  convolutional(input_data, (1, 1, 1024, 256), downsample=False, activate=True, bn=True)
+
+    conv_1 = tf.keras.layers.SeparableConv2D(conv, kernel_size=(3, 3), strides=1,padding="same")
+
+    conv_1_1 = conv2d_bn(conv_1, (256,256), 3, 3, dilation_rate=3)
+
+    conv_2_1 = conv2d_bn(conv_1,(256,256), 3,3, dilation_rate=5)
+
+    output = tf.concat([conv_1, conv_1_1, conv_2_1, conv], axis=3)
+
+    output = convolutional(output, (1, 1, 1024, 1024), downsample=False, activate=False, bn=True)
+
+    output = output + input_data
+
+    output  = tf.nn.leaky_relu(output, alpha=0.1)
+
+    return output
+
+def cbam_block(input_feature, name, ratio=8):
+    """Contains the implementation of Convolutional Block Attention Module(CBAM) block.
+    As described in https://arxiv.org/abs/1807.06521.
+    """
+
+    with tf.variable_scope(name):
+        attention_feature = channel_attention(input_feature, 'ch_at', ratio)
+        attention_feature = spatial_attention(attention_feature, 'sp_at')
+        # print("CBAM Hello")
+    return attention_feature
+
+
+def channel_attention(input_feature, name, ratio=8):
+    kernel_initializer = tf.contrib.layers.variance_scaling_initializer()
+    bias_initializer = tf.constant_initializer(value=0.0)
+
+    with tf.variable_scope(name):
+        channel = input_feature.get_shape()[-1]
+        avg_pool = tf.reduce_mean(input_feature, axis=[1, 2], keepdims=True)
+
+        assert avg_pool.get_shape()[1:] == (1, 1, channel)
+        avg_pool = tf.layers.dense(inputs=avg_pool,
+                                   units=channel // ratio,
+                                   activation=tf.nn.relu,
+                                   kernel_initializer=kernel_initializer,
+                                   bias_initializer=bias_initializer,
+                                   name='mlp_0',
+                                   reuse=None)
+        assert avg_pool.get_shape()[1:] == (1, 1, channel // ratio)
+        avg_pool = tf.layers.dense(inputs=avg_pool,
+                                   units=channel,
+                                   kernel_initializer=kernel_initializer,
+                                   bias_initializer=bias_initializer,
+                                   name='mlp_1',
+                                   reuse=None)
+        assert avg_pool.get_shape()[1:] == (1, 1, channel)
+
+        max_pool = tf.reduce_max(input_feature, axis=[1, 2], keepdims=True)
+        assert max_pool.get_shape()[1:] == (1, 1, channel)
+        max_pool = tf.layers.dense(inputs=max_pool,
+                                   units=channel // ratio,
+                                   activation=tf.nn.relu,
+                                   name='mlp_0',
+                                   reuse=True)
+        assert max_pool.get_shape()[1:] == (1, 1, channel // ratio)
+        max_pool = tf.layers.dense(inputs=max_pool,
+                                   units=channel,
+                                   name='mlp_1',
+                                   reuse=True)
+        assert max_pool.get_shape()[1:] == (1, 1, channel)
+
+        scale = tf.sigmoid(avg_pool + max_pool, 'sigmoid')
+
+    return input_feature * scale
+
+
+def spatial_attention(input_feature, name):
+    kernel_size = 7
+    kernel_initializer = tf.contrib.layers.variance_scaling_initializer()
+    with tf.variable_scope(name):
+        avg_pool = tf.reduce_mean(input_feature, axis=[3], keepdims=True)
+        assert avg_pool.get_shape()[-1] == 1
+        max_pool = tf.reduce_max(input_feature, axis=[3], keepdims=True)
+        assert max_pool.get_shape()[-1] == 1
+        concat = tf.concat([avg_pool, max_pool], 3)
+        assert concat.get_shape()[-1] == 2
+
+        concat = tf.layers.conv2d(concat,
+                                  filters=1,
+                                  kernel_size=[kernel_size, kernel_size],
+                                  strides=[1, 1],
+                                  padding="same",
+                                  activation=None,
+                                  kernel_initializer=kernel_initializer,
+                                  use_bias=False,
+                                  name='conv')
+        assert concat.get_shape()[-1] == 1
+        concat = tf.sigmoid(concat, 'sigmoid')
+
+    return input_feature * concat
